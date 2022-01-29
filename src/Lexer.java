7d299073eda9cf831ae8f76ac46ceb48be2d6a13
Mikko <mikkotakala@hotmail.com>
2022-01-29 15:10:24 +0200

    Lexer indentifies numbers

--- src/Lexer.java
+++ src/Lexer.java
@@ -3,6 +3,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,6 +14,23 @@ public class Lexer {
     private int startPosition = 0;
     private int currentPosition = 0;
     private int lineNumber = 1;
+
+    private static HashMap<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("print", TokenType.PRINT);
+        keywords.put("var", TokenType.VAR);
+        keywords.put("for", TokenType.FOR);
+        keywords.put("end", TokenType.END);
+        keywords.put("in", TokenType.IN);
+        keywords.put("do", TokenType.DO);
+        keywords.put("read", TokenType.READ);
+        keywords.put("int", TokenType.INT);
+        keywords.put("string", TokenType.STRING);
+        keywords.put("bool", TokenType.BOOL);
+        keywords.put("assert", TokenType.ASSERT);
+    }
 
     public Lexer(String source) {
         this.source = source;
@@ -35,11 +53,16 @@ public class Lexer {
     private void scanToken() {
         char currentCharacter = getNextCharacter();
         switch (currentCharacter) {
+            case ' ':
+            case '\r':
+            case '\t': break;
+            case '\n': this.lineNumber++; break;
             case '(': addToken(TokenType.LEFT_PAREN); break;
             case ')': addToken(TokenType.RIGHT_PAREN); break;
             case '+': addToken(TokenType.PLUS); break;
             case '-': addToken(TokenType.MINUS); break;
             case '*': addToken(TokenType.STAR); break;
+            case '/': addToken(TokenType.SLASH); break;
             case ';': addToken(TokenType.SEMICOLON); break;
             case '.':
                 addToken(isNextCharacter('.') ? TokenType.SPREAD : TokenType.DOT);
@@ -49,22 +72,74 @@ public class Lexer {
                 addToken(isNextCharacter('=') ? TokenType.ASSIGN : TokenType.COLON);
                 break;
             case '"':
-                // It's always a string, but we must make sure it has closing quote too, else it's an error.
-                boolean foundClosingQuote = false;
-                while(peekNextCharacter() != '\0' && !isAtEnd()) {
-                    if(peekNextCharacter() == '"')  {
-                        foundClosingQuote = true;
-                        getNextCharacter();
-                        addToken(TokenType.STRING_LIT);
-                        break;
-                    }
-                    getNextCharacter();
+                handleStringLiteral();
+                break;
+            default:
+                if(this.isLowerCaseAlpha(currentCharacter)) {
+                    this.handleReservedKeyword();
+                    return;
                 }
-                // If we found a closing quote, break out of switch, otherwise fall through to error
-                if(foundClosingQuote) break;
-                // otherwise its a string
-            default: CompilerMain.error(lineNumber, "Unexpected token: " + currentCharacter);
+                if(this.isNumber(currentCharacter)) {
+                    this.handleNumber();
+                    return;
+                }
+                CompilerMain.error(lineNumber, "Unexpected token: " + currentCharacter);
         }
+    }
+
+    private void handleNumber() {
+        while(this.isNumber(this.peekNextCharacter()) && peekNextCharacter() != '\0' && !isAtEnd()) {
+            getNextCharacter();
+        }
+        addToken(TokenType.NUMBER);
+    }
+
+    private void handleReservedKeyword() {
+        // Read characters until a non-alpha or EOF is met.
+        while(this.isAlphabet(this.peekNextCharacter()) && peekNextCharacter() != '\0' && !isAtEnd()) {
+            getNextCharacter();
+        }
+        // Fetch from keyword hashmap with the result
+        if(keywords.get(this.source.substring(this.startPosition, this.currentPosition)) != null) {
+            addToken(keywords.get(this.source.substring(this.startPosition, this.currentPosition)));
+            return;
+        }
+        // NULL => not a keyword, error. Needs to be able to detect identifiers too!
+        CompilerMain.error(
+            this.lineNumber, "Unknown keyword: " +
+            this.source.substring(this.startPosition, this.currentPosition)
+        );
+        // Token returned -> addToken
+    }
+
+    private void handleStringLiteral() {
+        // It's always a string, but we must make sure it has closing quote too, else it's an error.
+        boolean foundClosingQuote = false;
+        while(peekNextCharacter() != '\0' && !isAtEnd()) {
+            if(peekNextCharacter() == '"')  {
+                foundClosingQuote = true;
+                getNextCharacter();
+                addToken(TokenType.STRING_LIT);
+                break;
+            }
+            if(peekNextCharacter() == '\n') this.lineNumber++;
+            getNextCharacter();
+        }
+        // If we found a closing quote, break out of switch, otherwise error
+        if(foundClosingQuote) return;
+        CompilerMain.error(this.lineNumber, "Unterminated string");
+    }
+
+    private boolean isNumber(char character) {
+        return character >= '0' && character <= '9';
+    }
+
+    private boolean isAlphabet(char character) {
+        return (character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z');
+    }
+
+    private boolean isLowerCaseAlpha(char character) {
+        return character >= 'a' && character <= 'z';
     }
 
     private char getNextCharacter() {
--- src/TokenType.java
+++ src/TokenType.java
@@ -1,14 +1,23 @@
 enum TokenType {
     LEFT_PAREN, RIGHT_PAREN,
-    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR, COLON, ASSIGN, SPREAD,
+    COMMA, DOT, SEMICOLON, COLON, ASSIGN, SPREAD,
+    
+    // Arithmetic
+    MINUS, PLUS, STAR, SLASH,
 
     // Logical operators
     EQUAL, GREATER, LESS, OR, AND,
 
-    IDENTIFIER, STRING_LIT,
+    // Literal values (strings, integers and booleans)
+    STRING_LIT, NUMBER, FALSE, TRUE,
+
+    // The 'names' of variables
+    IDENTIFIER,
 
     // Reserved keywords
-    VAR, FOR, END, IN, DO, READ, PRINT, BOOL, ASSERT, INT, STRING,
+    VAR, FOR, END, IN, DO, READ, PRINT, ASSERT,
+    // Types
+    INT, STRING, BOOL,
 
     EOF
 }
